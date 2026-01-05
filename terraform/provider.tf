07473c887c011e405183f22957196f90999f7c38
Nazariy Buryak <nazarburyak21@gmail.com>
2026-01-05 15:15:23 +0000

    Sorting + Terraform infra start

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.13.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
